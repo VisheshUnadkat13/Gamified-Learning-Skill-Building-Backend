a172da800583b6c7bca333ad71de488c1dd265f1
VisheshUnadkat13 <unadkatvishesh@gmail.com>
2026-01-18 23:37:14 +0530

    project Builder module start"

---
+++ GamifiedLearningPlatformBackend/src/main/java/com/GamifiedLearningPlatform/GamifiedLearningPlatformBackend/model/ProjectBuilder.java
@@ -0,0 +1,16 @@
+package com.GamifiedLearningPlatform.GamifiedLearningPlatformBackend.model;
+
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProjectBuilder {
+
+}
